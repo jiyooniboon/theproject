957dd4e9dd3151a49491167ae078e784b1add233
jiyooniboon <jiyooniboon@g.skku.edu>
2025-12-10 18:01:49 +0900

    great progress

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("start");
+
+const btn = document.getElementById("itm");
+
+btn.addEventListener("click", getname);
+
+function getname() {
+
+    console.log("code to add item to list");
+}
