7585154cd5940b5cd9cb146155b2375c8de66396
jiyooniboon <jiyooniboon@g.skku.edu>
2025-12-12 17:41:55 +0900

    add new items is smooth

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
 
-console.log("start");
+const addItemButton = document.getElementById("addItem");
+const activeItems = document.querySelectorAll("#active");
+const compItems = document.querySelectorAll("#done");
 
-const todoButton = document.getElementById("addNewTodo");
-const todoName = document.getElementById("name");
-const list = document.getElementById("curList");
+const current = document.querySelector("section.curList ul");
 
-const buttons = document.querySelector("button");
-buttons.addEventListener("click", checkTodo);
 
-todoButton.addEventListener("click", btnPressAdd);
+const todoName = document.createElement("input");
+todoName.id = "name";
+todoName.type = "text";
 
-function btnPressAdd() {
-    const checkBtn = document.createElement("button");
-    checkBtn.textContent = todoName.value;
-    list.appendChild(checkBtn);
 
-    todoName.value = "";
+addItemButton.addEventListener("click", getName);
+
+todoName.addEventListener("keydown", (e) => {
+    if (e.key == 'Enter') {
+        const lastItem = document.createElement("button");
+        lastItem.id = "active";
+        lastItem.innerText = todoName.value;
+
+        console.log(lastItem);
+        const lastItemLi = document.createElement("li");
+        lastItemLi.appendChild(lastItem);
+        current.appendChild(lastItemLi);
+
+        todoName.value = "";
+        todoName.hidden = true;
+        current.appendChild(addItemButton);
+        addItemButton.hidden = false;
+
+        console.log(current);
+
+    }
+
+});
+
+//activeItems.addEventListener("click", checkTodo);
+//compItems.addEventListener("click", uncheckTodo);
+
+function getName() {
+    // button turns into text input,
+    // when press enter in text input, run addItem
+    //addItemButton.hidden();
+    addItemButton.hidden = true;
+    
+    current.appendChild(todoName);
+    todoName.hidden = false;
+
+
+}
+
+function addItem() {
+
+
+
 }
 
 function checkTodo() {
-    console.log(buttons);
+    // delete button from curlist
+    // add it to complist 
+}
 
+function uncheckTodo() {
+    // delete button from complist
+    // add it to curlist
 }
