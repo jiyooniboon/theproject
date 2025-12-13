f9dd38bfd5609346d8f246806c8c5e9d351d8ad8
jiyooniboon <jiyooniboon@g.skku.edu>
2025-12-13 14:50:46 +0900

    add check and uncheck items

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
 
 const addItemButton = document.getElementById("addItem");
-const activeItems = document.querySelectorAll("#active");
-const compItems = document.querySelectorAll("#done");
-
 const current = document.querySelector("section.curList ul");
-
+const done = document.querySelector("section.compList ul");
 
 const todoName = document.createElement("input");
 todoName.id = "name";
 todoName.type = "text";
 
-
 addItemButton.addEventListener("click", getName);
 
+// listen for enter press, add new todo item to current list
 todoName.addEventListener("keydown", (e) => {
     if (e.key == 'Enter') {
-        const lastItem = document.createElement("button");
-        lastItem.id = "active";
-        lastItem.innerText = todoName.value;
 
-        console.log(lastItem);
-        const lastItemLi = document.createElement("li");
-        lastItemLi.appendChild(lastItem);
-        current.appendChild(lastItemLi);
+        if (todoName.value == "") {
+            // switch back if no input
+            current.replaceChild(addItemButton, todoName);
+        } else {
+            const lastItem = document.createElement("button");
+            lastItem.id = "active";
+            lastItem.innerText = todoName.value;
 
-        todoName.value = "";
-        todoName.hidden = true;
-        current.appendChild(addItemButton);
-        addItemButton.hidden = false;
+            current.replaceChild(lastItem, todoName);
 
-        console.log(current);
+            todoName.value = "";
+            current.appendChild(addItemButton);
+        }
+    }
+});
+
+// button turns into text input 
+// text input listens for enter keydown
+function getName() {
+    current.replaceChild(todoName, addItemButton);
+    todoName.focus();
+}
+
+// not in use
+function addItem() {
+    const lastItem = document.createElement("button");
+    lastItem.id = "active";
+    lastItem.innerText = todoName.value;
+
+    const lastItemLi = document.createElement("li");
+    lastItemLi.appendChild(lastItem);
+    current.replaceChild(lastItemLi, todoName);
+
+    todoName.value = "";
+    current.appendChild(addItemButton);
+}
+
+current.addEventListener("click", (e) => {
+    if (e.target.id == "active") {
+        e.target.id = "done";
+        current.removeChild(e.target);
+        done.appendChild(e.target);
+    }
+
+})
+
+done.addEventListener("click", (e) => {
+    if (e.target.id == "done") {
+        e.target.id = "active";
+
+        current.insertBefore(e.target, addItemButton);
 
     }
 
-});
-
-//activeItems.addEventListener("click", checkTodo);
-//compItems.addEventListener("click", uncheckTodo);
-
-function getName() {
-    // button turns into text input,
-    // when press enter in text input, run addItem
-    //addItemButton.hidden();
-    addItemButton.hidden = true;
-    
-    current.appendChild(todoName);
-    todoName.hidden = false;
-
-
-}
-
-function addItem() {
-
-
-
-}
-
-function checkTodo() {
-    // delete button from curlist
-    // add it to complist 
-}
-
-function uncheckTodo() {
-    // delete button from complist
-    // add it to curlist
-}
+})
