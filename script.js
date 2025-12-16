e990e44a23c3cd9254f9e914d5f7e212e73ca8d2
jiyooniboon <jiyooniboon@g.skku.edu>
2025-12-16 17:17:49 +0900

    weekly layout todo list

--- script.js
+++ script.js
@@ -1,70 +1,54 @@
 
-const addItemButton = document.getElementById("addItem");
-const current = document.querySelector("section.curList ul");
-const done = document.querySelector("section.compList ul");
 
 const todoName = document.createElement("input");
-todoName.id = "name";
 todoName.type = "text";
 
-addItemButton.addEventListener("click", getName);
-
-// listen for enter press, add new todo item to current list
-todoName.addEventListener("keydown", (e) => {
-    if (e.key == 'Enter') {
-
-        if (todoName.value == "") {
-            // switch back if no input
-            current.replaceChild(addItemButton, todoName);
-        } else {
-            const lastItem = document.createElement("button");
-            lastItem.id = "active";
-            lastItem.innerText = todoName.value;
-
-            current.replaceChild(lastItem, todoName);
-
-            todoName.value = "";
-            current.appendChild(addItemButton);
-        }
+const allItems = document.querySelector("div.container");
+allItems.addEventListener("click", (e) => {
+    if (e.target.id == "addItem") {
+        e.target.replaceWith(todoName);
+        todoName.focus();
     }
-});
 
-// button turns into text input 
-// text input listens for enter keydown
-function getName() {
-    current.replaceChild(todoName, addItemButton);
-    todoName.focus();
-}
-
-// not in use
-function addItem() {
-    const lastItem = document.createElement("button");
-    lastItem.id = "active";
-    lastItem.innerText = todoName.value;
-
-    const lastItemLi = document.createElement("li");
-    lastItemLi.appendChild(lastItem);
-    current.replaceChild(lastItemLi, todoName);
-
-    todoName.value = "";
-    current.appendChild(addItemButton);
-}
-
-current.addEventListener("click", (e) => {
     if (e.target.id == "active") {
         e.target.id = "done";
-        current.removeChild(e.target);
-        done.appendChild(e.target);
+    } else if (e.target.id == "done") {
+        e.target.id = "active";
     }
 
 })
 
-done.addEventListener("click", (e) => {
-    if (e.target.id == "done") {
-        e.target.id = "active";
+function addTodo(e) {
+    const addItemButton = document.createElement("button");
+    addItemButton.innerHTML = "New Todo";
+    addItemButton.id = "addItem";
 
-        current.insertBefore(e.target, addItemButton);
+
+    if (todoName.value == "") {
+        todoName.replaceWith(addItemButton);
+
+    } else {
+        const lastItem = document.createElement("button");
+        lastItem.id = "active";
+        lastItem.innerText = todoName.value;
+
+        todoName.replaceWith(lastItem);
+
+        todoName.value = "";
+        lastItem.after(addItemButton);
+    }
+
+}
+
+// listen for enter press, add new todo item to current list
+todoName.addEventListener("keydown", (e) => {
+    if (e.key == 'Enter') {
+        addTodo(e);
 
     }
+});
+
+todoName.addEventListener("blur", (e) => {
+    addTodo(e);
 
 })
